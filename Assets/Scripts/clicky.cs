18332fc2e5b190184785f4315243985f8678023d
Jake <jdragonrider@hotmail.com>
2020-12-03 19:22:10 -0700

    Clicky sound.

---
+++ Assets/Scripts/clicky.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+public class clicky : MonoBehaviour
+{
+    private AudioSource clickSound;
+
+    private void Start()
+    {
+        clickSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        clickSound.Play();
+    }
+}
